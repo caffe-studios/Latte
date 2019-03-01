3e9a9520437d3e6789d9af87ea89084991c973be
Nick Ruspantini <nruspantini@ufl.edu>
2019-02-28 23:22:58 -0500

    Logging

--- Latte/src/Latte.h
+++ Latte/src/Latte.h
@@ -3,7 +3,7 @@
 // For use by Latte Apps
 
 #include "Latte/Application.h"
+#include "Latte/Log.h"
 
 // --- ENTRY POINT ---------------------
 #include "Latte/EntryPoint.h"
-
--- Latte/src/Latte/EntryPoint.h
+++ Latte/src/Latte/EntryPoint.h
@@ -4,7 +4,13 @@
 
 extern Latte::Application* Latte::CreateApplication();
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
+	Latte::Log::Init();
+	LT_CORE_WARN("Initialized Log!");
+	int a = 5;
+	LT_INFO("Hello! Var={0}", a);
+
 	auto app = Latte::CreateApplication();
 	app->Run();
 	delete app;
