247ce3438e8cba855c2664477ced1384d1cc731a
Nick <nruspantini@ufl.edu>
2019-03-26 23:03:52 -0400

    clion init
    hello world

---
+++ cmake-build-debug/CMakeFiles/Latte.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nickruspantini/Latte/src/main.cpp" "/Users/nickruspantini/Latte/cmake-build-debug/CMakeFiles/Latte.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/nickruspantini/Library/Frameworks/SDL2.framework/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/SDL2_App.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL2_App.dir/src/main.cpp.o"
+  "SDL2_App.pdb"
+  "SDL2_App"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL2_App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
