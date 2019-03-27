ad8d5a01962bb7637b416bcbcc62404294c2d820
Nick Ruspantini <nruspantini@ufl.edu>
2019-03-26 22:18:38 -0400

    Reset: goals changed. new repo new ,e

--- Latte/src/Latte.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// For use by Latte Apps
-
-#include "Latte/Application.h"
-#include "Latte/Log.h"
-
-// --- ENTRY POINT ---------------------
-#include "Latte/EntryPoint.h"
--- README.md
+++
@@ -1,2 +0,0 @@
-# Latte
-Lattè Engine
