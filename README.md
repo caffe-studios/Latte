f9e8f783198cc33fbaa1c3de6c89431cb7a083f5
Tony Faieta <tfaieta@gmail.com>
2019-03-31 19:36:56 -0400

    add(example): finished TealWindow example + updated readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project has multiple folders that have examples of how Wasm works with
+This project has multiple folders that have examples of how Wasm works with Emscripten in a couple of different languages (C++, Rust, Go). You'll need to have [emscripten](https://github.com/emscripten-core/emscripten) installed to run these examples.
 
 ## RotatingTriangle
 
@@ -15,3 +15,9 @@ You can see the current version live [here](https://latte.tfaieta.now.sh/).
 ## TealWindow
 
 Built using C++ & GLFW
+
+- Build: `make`
+- Clean: `make clean`
+- Build, but remove objects leaving the build dir: `make dist`
+
+You can then use `python -m SimpleHTTPServer 8080` and open your browser to [http://localhost:8080/build/](http://localhost:8080/build/).
--- TealWindow/src/main.c
+++ TealWindow/src/main.c
@@ -1,49 +1,44 @@
 #include <emscripten/emscripten.h>
 #define GLFW_INCLUDE_ES3
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 GLFWwindow *window;
-
-// NOT DONE YET, GETTING CONSOLE ERROR
 
 static void generate_frame()
 {
   float ratio;
   int width, height;
   glfwGetFramebufferSize(window, &width, &height);
-  ratio = width / (float)height;
-  glViewport(0, 0, width, height);
-  glClear(GL_COLOR_BUFFER_BIT);
-
   glfwSwapBuffers(window);
   glfwPollEvents();
 }
 
 int main()
 {
-  window = glfwCreateWindow(320, 240, "Hello World", NULL, NULL);
-
   if (!glfwInit())
-    return -1;
+  {
+    fputs("Faileid to initialize GLFW", stderr);
+    emscripten_force_exit(EXIT_FAILURE);
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+  window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+
+  glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
 
   if (!window)
   {
+    fputs("Failed to create GLFW window", stderr);
     glfwTerminate();
-    return -1;
+    emscripten_force_exit(EXIT_FAILURE);
   }
 
   glfwMakeContextCurrent(window);
 
-  glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
-
-  while (!glfwWindowShouldClose(window))
-  {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-
   emscripten_set_main_loop(generate_frame, 0, 0);
-
-  return 0;
 }
